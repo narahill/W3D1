f070ddd33d7eb25502ed61ce6932d64dd2409230
Nara Hillyer <nara@hillyer.cc>
2022-08-01 13:32:46 -0700

    added my any

--- enumerables.rb
+++ enumerables.rb
@@ -16,6 +16,23 @@ class Array
         self.my_each { |ele| new_array << ele if prc.call(ele) == true}
         new_array
     end
+     
+    def my_reject(&prc)
+        new_array=[]
+        self.my_each{ |ele| new_array<< ele if prc.call(ele)==false}
+        new_array
+    end 
+
+    def my_any?(&prc)
+        self.my_each{|ele| return true if prc.call(ele)}
+        return false 
+    end 
+
+    def my_all?(&prc)
+        self.my_each{|ele| return false if !prc.call(ele)}
+        return true
+    end 
+
 
 
 end 
